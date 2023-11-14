a2c2ae6a6dfa8afd2e8abe33a335f1e6cfc19e05
Rebelo7 <143350138+Rebelo7@users.noreply.github.com>
2023-11-14 20:38:17 +0000

    add todo with wrong background

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -9,21 +9,32 @@ const Home = () => {
    setTask('');
  }
 
+ const handleKeyDown = (event) => {
+  if(event.key === 'Enter'){
+    addTask();
+  }
+  }
+
  return (
-	<div className='todo-list'>
-		<h3>Be Productive !</h3>
-     <input 
-	  className='taskInput'
-       type="text" 
-       value={task} 
-       onChange={e => setTask(e.target.value)} 
-       placeholder="Task for today" 
-     />
-     <button onClick={addTask}>Add Task</button>
-     <ul>
-       {tasks.map((task, index) => <li key={index}>{task}<i class="fa-solid fa-trash"></i></li>)}
-     </ul>
-   </div>
+  <div>
+	  <div className='todo-list'>
+		  <h3>Be Productive !</h3>
+      <input 
+        className='taskInput'
+        type="text" 
+        onChange={e => setTask(e.target.value)} 
+        value={task} 
+        placeholder="Task for today"
+        onKeyDown={handleKeyDown}
+         
+      />
+      <button onClick={addTask}>Add Task</button>
+      <ul>
+       {tasks.map((task, index) => <li key={index}>{task}<i class="fa-solid fa-trash" onClick={() => setTasks(tasks.filter((task, currenTask) => index !== currenTask))} ></i></li>)}
+      </ul>
+    </div>
+     <div className='tasksRemaining'>{tasks.length}Tasks Remaining</div>
+  </div>
  );
 }
 
