2ec3be16640a21a01d89562e24442936a4fd0d5d
Rebelo7 <143350138+Rebelo7@users.noreply.github.com>
2023-11-20 20:12:27 +0000

    set fetch from API to the TODO

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,42 +1,74 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Home = () => {
  const [task, setTask] = useState('');
  const [tasks, setTasks] = useState([]);
+ const DOMAIN = "https://playground.4geeks.com/apis/fake/todos/user/Rebelo7";
+
+ const handleFetchTasks = async () => {
+ const textResponse = await fetch(DOMAIN);
+ const jsonResponse = await textResponse.json();
+ setTasks(jsonResponse);
+}
+
+ useEffect(() => {
+ handleFetchTasks(); 
+}, []);
+
+ async function updateTask(updatedList) {
+ 
+   const response = await fetch(DOMAIN, {
+     method: 'PUT',
+     headers: {
+       "Content-Type": "application/json"
+     },
+     body: JSON.stringify(updatedList), 
+   });
+     handleFetchTasks(); 
+ }
+    
 
  const addTask = () => {
-   setTasks([...tasks, task]);
-   setTask('');
+  const newTask = { label: task, done: false };
+  const updatedList = [...tasks, newTask ]; 
+  setTasks(updatedList);
+  setTask('');
  }
 
  const handleKeyDown = (event) => {
-  if(event.key === 'Enter'){
-    addTask();
-  }
-  }
+ if(event.key === 'Enter'){
+   addTask();
+ }
+ }
+
+ const handleDelete = (index) => {
+  const updatedList = tasks.filter((task, currenTask) => index !== currenTask);
+  setTasks(updatedList, () => {
+   updateTask(updatedList);
+  });
+ }
+ 
 
  return (
-  <div>
-	  <div className='todo-list'>
-		  <h3>Be Productive !</h3>
-      <input 
-        className='taskInput'
-        type="text" 
-        onChange={e => setTask(e.target.value)} 
-        value={task} 
-        placeholder="Task for today"
-        onKeyDown={handleKeyDown}
-      />
-      <button onClick={addTask}>Add Task</button>
-      <ul>
-       {tasks.map((task, index) => <li key={index}>{task}<i class="fa-solid fa-trash" onClick={() => setTasks(tasks.filter((task, currenTask) => index !== currenTask))} ></i></li>)}
-      </ul>
-    </div>
-     <div className='tasksRemaining'>{tasks.length} Tasks Remaining</div>
-  </div>
+ <div>
+	 <div className='todo-list'>
+		 <h3>Be Productive !</h3>
+     <input 
+       className='taskInput'
+       type="text" 
+       onChange={e => setTask(e.target.value)} 
+       value={task} 
+       placeholder="Task for today"
+       onKeyDown={handleKeyDown}
+     />
+     <button onClick={addTask}>Add Task</button>
+     <ul>
+      {tasks.map((task, index) => <li key={index}>{task.label}<i class="fa-solid fa-trash" onClick={() => handleDelete(index)} ></i></li>)}
+     </ul>
+   </div>
+    <div className='tasksRemaining'>{tasks.length} Tasks Remaining</div>
+ </div>
  );
 }
 
 export default Home;
-
-
